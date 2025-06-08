a0de82096059ce83563bd731371fdd40d9d443f6
aayush-bounteous <Aayush.Gupta@bounteous.com>
2025-06-09 00:44:36 +0530

    a

--- Library_Managament_System/src/repository/Repository.java
+++ Library_Managament_System/src/repository/Repository.java
@@ -6,7 +6,7 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class Repository<T> {
-    private final List<T> items;
+   final List<T> items;
 
     public Repository() {
         this.items = new ArrayList<>();
@@ -24,19 +24,21 @@ public class Repository<T> {
 
     // Get all items
     public List<T> getAll() {
-        return new ArrayList<>(items); // return a copy to prevent external modification
+        return new ArrayList<>(items);
     }
 
-    // Find first item matching a predicate
+    //finding the first item that matches the given condition.
     public T find(Predicate<T> condition) {
         return items.stream().filter(condition).findFirst().orElse(null);
     }
 
+    //predicate -  it takes an input of type T and returns a boolean.
     // Get all items matching a condition
     public List<T> filter(Predicate<T> condition) {
         return items.stream().filter(condition).collect(Collectors.toList());
     }
 
+    //this is inbuilt feature of java for clearing for clearing the list
     // Clear all items
     public void clear() {
         items.clear();
